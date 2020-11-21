b876206fd479a73ce6d5efd1d2f9a5ea29415d73
Harvey1024 <hw-li@foxmail.com>
2020-11-21 20:56:29 +0800

    new project

---
+++ src/test/java/Harvey/VideoShare/VideoShareApplicationTests.java
@@ -0,0 +1,13 @@
+package Harvey.VideoShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
